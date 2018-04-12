6cf022380e30cb2b44d648a689d7a8a047b5de4f
liyuan <707335344@qq.com>
2018-04-12 11:42:10 +0800

    微信部分调试通过

---
+++ src/main/java/com/liyuan/demo/constants/ConstantUtils.java
@@ -0,0 +1,11 @@
+package com.liyuan.demo.constants;
+/**
+ * 项目常量类
+ * @author Administrator
+ *
+ */
+public class ConstantUtils {
+	//项目域名地址
+	public static final String serverPath = "http://www.dyjkglass.com/";
+	
+}
